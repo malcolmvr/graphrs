d1103e00fa6565cb29366228331e281158369196
Malcolm van Raalte <malcolm@van.raalte.ca>
2021-01-01 10:42:21 -0500

    DiGraph::new

--- tests/test_digraph.rs
+++ tests/test_digraph.rs
@@ -1,24 +1,31 @@
 #[cfg(test)]
 mod tests {
 
-    use graphx::{DiGraph,NodeAttributes};
+    use std::collections::HashMap;
+    use graphx::{DiGraph};
     
     #[test]
-    fn test_add_node() {
-        let mut g = DiGraph::new();
-        let g = g.add_node("n1", None);
-        let attr = g.get_node_attributes("n1");
-        assert_eq!(attr.attributes.len(), 0);
-        let mut attr = NodeAttributes::new();
-        attr.attributes.insert("a", &1.0);
-        let g = g.add_node("n1", Some(attr));
-        let attr = g.get_node_attributes("n1");
-        assert_eq!(attr.attributes.len(), 1);
-        let mut attr = NodeAttributes::new();
-        attr.attributes.insert("b", &2.0);
-        let g = g.add_node("n1", Some(attr));
-        let attr = g.get_node_attributes("n1");
-        assert_eq!(attr.attributes.len(), 2);
+    fn test_digraph_new() {
+
+        let mut node1attr = HashMap::new();
+        node1attr.insert("weight", &1.0);
+
+        let mut node2attr = HashMap::new();
+        node2attr.insert("weight", &2.0);
+
+        let mut node_vec = Vec::new();
+        node_vec.push(("n1", node1attr));
+        node_vec.push(("n2", node2attr));
+
+        let mut edges_vec = Vec::new();
+        edges_vec.push(("n1", "n2"));
+        edges_vec.push(("n2", "n1"));
+
+        let graph = DiGraph::new(node_vec, edges_vec);
+
+        assert_eq!(graph.nodes.len(), 2);
+        assert_eq!(graph.edges.len(), 2);
+
     }
     
 }
