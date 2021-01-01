6c98790382d87244114bd294dc5ffcd95ebbe977
Malcolm van Raalte <malcolm@van.raalte.ca>
2021-01-01 12:59:55 -0500

    immutable nodes/edges

--- tests/test_digraph.rs
+++ tests/test_digraph.rs
@@ -14,17 +14,19 @@ mod tests {
         node2attr.insert("weight", &2.0);
 
         let mut node_vec = Vec::new();
-        node_vec.push(("n1", node1attr));
-        node_vec.push(("n2", node2attr));
+        node_vec.push(("n1", Some(node1attr)));
+        node_vec.push(("n2", Some(node2attr)));
+        node_vec.push(("n3", None));
 
         let mut edges_vec = Vec::new();
         edges_vec.push(("n1", "n2"));
         edges_vec.push(("n2", "n1"));
+        edges_vec.push(("n1", "n3"));
 
         let graph = DiGraph::new(node_vec, edges_vec);
 
-        assert_eq!(graph.nodes.len(), 2);
-        assert_eq!(graph.edges.len(), 2);
+        assert_eq!(graph.nodes().len(), 3);
+        assert_eq!(graph.edges().len(), 3);
 
     }
     
