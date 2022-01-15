b9d8302868a8eb01e3399add945a4b9f068356e5
Malcolm van Raalte <malcolm@van.raalte.ca>
2022-01-15 07:05:31 -0500

    remove karate.graphml

--- tests/test_readwrite_graphml.rs
+++ tests/test_readwrite_graphml.rs
@@ -6,19 +6,23 @@ mod tests {
     #[test]
     fn test_write_then_read_graphml() {
         
+        let file = "./tests/karate.graphml";
+
         // write
         let graph = generators::social::karate_club_graph();
-        let result = readwrite::graphml::write_graphml(&graph, "./tests/files/karate.graphml");
+        let result = readwrite::graphml::write_graphml(&graph, file);
         assert!(result.is_ok());
 
         // read
-        let result = readwrite::graphml::read_graphml("./tests/files/karate.graphml", GraphSpecs::undirected());
+        let result = readwrite::graphml::read_graphml(file, GraphSpecs::undirected());
         assert!(result.is_ok());
         let graph = result.unwrap();
         let nodes = graph.get_all_nodes();
         assert_eq!(nodes.len(), 34);
         let edges = graph.get_all_edges();
         assert_eq!(edges.len(), 78);
+
+        assert!(std::fs::remove_file(file).is_ok());
     }
 
 }
